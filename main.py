450b8eb3a07044383cf549b2c475442f9744f231
Dimo Dimchev <ddimchev7@gmail.com>
2021-02-15 15:44:12 +0200

    Update main.py
    Implement a loop that allows the program to read more than one cards

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import serial
 
-# empty string that will contain the card 
-tag_id = ""
-ser = serial.Serial('/dev/cu.usbserial-A703G1E6')       
-# for loop that will receive all 16 bites from the card reader   
-for i in range(16):
-    read_data = ser.read()
-    # decodes the bytes to strings
-    char = read_data.decode("utf-8")
-    # concatenates the current byte to the rest of the bytes
-    tag_id += char
-# closes the port
-ser.close() 
-print(tag_id)
+# while loop that will allow for the program to repeat itself an indefinite amount
+while True:
+    # empty string that will contain the card 
+    tag_id = ""
+    ser = serial.Serial('/dev/cu.usbserial-A703G1E6')       
+    # for loop that will receive all 16 bites from the card reader   
+    for i in range(16):
+        read_data = ser.read()
+        # decodes the bytes to strings
+        char = read_data.decode("utf-8")
+        # concatenates the current byte to the rest of the bytes
+        tag_id += char
+    # closes the port
+    ser.close() 
+    print(tag_id)
