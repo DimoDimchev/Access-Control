5d8f75378ebec3fe3081a3727f783ead14124a12
Dimo Dimchev <ddimchev7@gmail.com>
2021-02-15 16:41:50 +0200

    Update main.py
    Fixed errors and implemented timeout and .in_waiting()

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 import serial
 
+ser = serial.Serial('/dev/cu.usbserial-A703G1E6', baudrate=9600, timeout=0.03)    
+
+
 # while loop that will allow for the program to repeat itself an indefinite amount
 while True:
+
     # empty string that will contain the card 
-    tag_id = ""
-    ser = serial.Serial('/dev/cu.usbserial-A703G1E6')       
-    # for loop that will receive all 16 bites from the card reader   
-    for i in range(16):
-        read_data = ser.read()
-        # decodes the bytes to strings
-        char = read_data.decode("utf-8")
-        # concatenates the current byte to the rest of the bytes
-        tag_id += char
-    # closes the port
-    ser.close() 
-    print(tag_id)
+    tag_id = ""   
+    card_id_size = ser.in_waiting()
+
+    read_data = ser.read(card_id_size)
+    # decodes the bytes to strings
+    char = read_data.decode("utf-8")
+
+    # concatenates the current byte to the rest of the bytes
+    tag_id += char
+
+# closes the port
+ser.close() 
+
+print(tag_id)
