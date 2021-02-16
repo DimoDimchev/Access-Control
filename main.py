684cfdc9bd1108b6e6e3764572247f8361e01c25
Dimo Dimchev <ddimchev7@gmail.com>
2021-02-16 14:32:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,24 @@
 import serial
 
 
-def check_if_closed():
+def close():
     if ser.is_open():
         ser.close()
-    else:
-        ser.open()
+
 
 
 def get_card_id():
-    # card_id_size = ser.in_waiting
-    read_data = ser.read(size=19)
+    card_id_size = ser.in_waiting
+    read_data = ser.read(card_id_size)
 
     # decodes the bytes to strings
     card_id = str(read_data, 'utf-8')
     card_id = card_id.encode("unicode_escape").decode("utf-8")
 
     # strips string from "?", "\n" and "\r"
-    for char in card_id:
-        if char in "?\\n\\r":
-            card_id = card_id.replace(char, '')
+    card_id = card_id.replace("?", '')
+    card_id = card_id.replace("\\n", '')
+    card_id = card_id.replace("\\r", '')
 
     return card_id
 
@@ -29,9 +28,9 @@ ser = serial.Serial('/dev/cu.usbserial-A703G1E6', baudrate=9600, timeout=0.1)
 
 # while loop that will allow for the program to repeat itself an indefinite amount
 while True:
-    if ser.in_waiting:
+    if ser.in_waiting == 19:
         # gets the card id
         print(get_card_id())
 
 # closes the port if it's open
-check_if_closed()
+close()
