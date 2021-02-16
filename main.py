f40a079702a574e3c3bde97035ee14a4a60b1a36
Dimo Dimchev <ddimchev7@gmail.com>
2021-02-16 17:27:27 +0200

    Update main.py
    Transformed program to OOP and integrated support for UniPi controller

--- main.py
+++ main.py
@@ -1,36 +1,43 @@
 import serial
+import requests
+import time
+
+class Reader:
+    def __init__(self):
+        self.ser = serial.Serial('/dev/cu.usbserial-A703G1E6', baudrate=9600, timeout=0.1)
+        self.card_id = None
+    
+
+    def close(self):
+        if self.ser.is_open():
+            self.ser.close()
+        
+
+    def get_card_id(self):
+        card_id_size = self.ser.in_waiting
+
+        if card_id_size == 19:
+            read_data = self.ser.read(card_id_size)
+
+            # decodes the bytes to strings
+            self.card_id = str(read_data, 'utf-8')
+            self.card_id = self.card_id.encode("unicode_escape").decode("utf-8")
+
+            # strips string from "?", "\n" and "\r"
+            self.card_id = self.card_id.replace("?", '')
+            self.card_id = self.card_id.replace("\\n", '')
+            self.card_id = self.card_id.replace("\\r", '')
+
+            return self.card_id
 
 
-def close():
-    if ser.is_open():
-        ser.close()
+reader_test = Reader()
 
-
-
-def get_card_id():
-    card_id_size = ser.in_waiting
-    read_data = ser.read(card_id_size)
-
-    # decodes the bytes to strings
-    card_id = str(read_data, 'utf-8')
-    card_id = card_id.encode("unicode_escape").decode("utf-8")
-
-    # strips string from "?", "\n" and "\r"
-    card_id = card_id.replace("?", '')
-    card_id = card_id.replace("\\n", '')
-    card_id = card_id.replace("\\r", '')
-
-    return card_id
-
-
-# opens port upon declaration
-ser = serial.Serial('/dev/cu.usbserial-A703G1E6', baudrate=9600, timeout=0.1)
-
-# while loop that will allow for the program to repeat itself an indefinite amount
 while True:
-    if ser.in_waiting == 19:
-        # gets the card id
-        print(get_card_id())
-
-# closes the port if it's open
-close()
+    card_id = reader_test.get_card_id()
+    if card_id == "6E536046010080FF":
+        # here will be the code that controls UniPy Neuron S103
+        url = "http://192.168.0.53:8080/rest/output/1_01"
+        response = requests.post(url, data={"value": 1, "mode": "Simple"})
+        time.sleep(5)
+        response = requests.post(url, data={"value": 0, "mode": "Simple"})
