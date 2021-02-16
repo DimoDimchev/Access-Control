8c25c001ac98f9df7c9683ff2195b53f9b15ad1f
Dimo Dimchev <ddimchev7@gmail.com>
2021-02-16 03:43:34 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import serial
 
+
 def check_if_closed():
     if ser.is_open():
         ser.close()
     else:
         ser.open()
+
 
 def get_card_id():
     card_id_size = ser.in_waiting
@@ -12,11 +14,12 @@ def get_card_id():
 
     # decodes the bytes to strings
     card_id = str(read_data, 'utf-8')
-    
+    card_id = card_id.encode("unicode_escape").decode("utf-8")
+
     # strips string from "?", "\n" and "\r"
     for char in card_id:
-        if char in "?\n\r":
-            card_id = card_id.replace(char,'')
+        if char in "?\\n\\r":
+            card_id = card_id.replace(char, '')
 
     return card_id
 
@@ -25,7 +28,7 @@ def get_card_id():
 ser = serial.Serial('/dev/cu.usbserial-A703G1E6', baudrate=9600, timeout=0.03)
 
 # while loop that will allow for the program to repeat itself an indefinite amount
-while True:  
+while True:
     if ser.in_waiting:
         # gets the card id
         print(get_card_id())
