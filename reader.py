0f564394fb9d0f0b19348272995c987ce3cd6658
Dimo Dimchev <ddimchev7@gmail.com>
2021-02-17 17:39:27 +0200

    Create reader.py
    Moved Reader class from main.py to reader.py

---
+++ reader.py
@@ -0,0 +1,33 @@
+#!/~/.pyenv/versions/3.9.1/bin/python
+# -*- coding: <utf-8> -*-
+
+import serial
+
+class Reader:
+    def __init__(self):
+        self.ser = serial.Serial('/dev/cu.usbserial-A703G1E6', baudrate=9600, timeout=0.1)
+        self.card_id = None
+    
+
+    def close(self):
+        if self.ser.is_open():
+            self.ser.close()
+        
+
+    def get_card_id(self):
+        card_id_size = self.ser.in_waiting
+
+        if card_id_size == 19:
+            read_data = self.ser.read(card_id_size)
+
+            # decodes the bytes to strings
+            self.card_id = str(read_data, 'utf-8')
+            self.card_id = self.card_id.encode("unicode_escape").decode("utf-8")
+
+            # strips string from "?", "\n" and "\r"
+            self.card_id = self.card_id.replace("?", '')
+            self.card_id = self.card_id.replace("\\n", '')
+            self.card_id = self.card_id.replace("\\r", '')
+
+            return self.card_id
+
