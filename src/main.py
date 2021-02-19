18399c2739b67bba8a8b3e650bcb42c19c2203de
Dimo Dimchev <ddimchev7@gmail.com>
2021-02-19 16:04:32 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -20,11 +20,22 @@ def main():
     card_list = parsed_data.get("cards_with_access")
 
     # read the port that will be used from the data.yaml file and save it in a variable
-    port_to_use = parsed_data.get("port")
+    controller_port = parsed_data.get("controller_port")
 
-    controller = Controller(ip_to_use, 1, 1)
-    reader = Reader(port_to_use)
-    timer = Timer(5)
+    # read the expiration time from the data.yaml file and save it in a variable
+    expiration_time = int(parsed_data.get("expiration_time"))
+
+    # read the major and minor index for the circuit from the data.yaml file and save them to variables
+    major_index = int(parsed_data.get("major_index"))
+    minor_index = int(parsed_data.get("minor_index"))
+
+    # read the network port from the data.yaml file and save it to a variable
+    network_port = str(parsed_data.get("network_port"))
+
+
+    controller = Controller(ip_to_use, major_index, minor_index, network_port)
+    reader = Reader(controller_port)
+    timer = Timer(expiration_time)
 
     # while loop to keep the program running indefinetely
     while True:
